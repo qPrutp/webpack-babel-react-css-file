a3c88f6b16030e2f5ffd427434cb86dbaad4a103
Vasyl Savitskyy <vasylsavitskyy@gmail.com>
2019-12-06 22:44:54 +0200

    add config/webpack.build.apache.conf.js,  config/constans/core.js

--- config/webpack.build.conf.js
+++ config/webpack.build.conf.js
@@ -1,32 +1,15 @@
 const merge = require('webpack-merge');
 const baseWebpackConfig = require('./webpack.base.conf');
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { publicPath } = require('./constans/core');
 
 const buildWebpackConfig = merge(baseWebpackConfig, {
     mode: 'production',
     output: {
-        /*
-            * якщо потрібно абсолютний шлях до імтортованих файлів js та css в index.html
-            * значення publicPath: '/', якщо відносне publicPath: './'
-            * ячкщо проект розгортається на Apache publicPath: '/', в іншову випадку буде помилка три роутах з параметрами
-            * параметр продубльований з webpack.base.conf оскільки при dev розробці потрібно значення publicPath: '/'
-        */
-        publicPath: '/',
+        publicPath,
     },
     plugins: [
-        // This plugin will delete all files inside `output.path` (the dist directory),
-        // but the directory itself will be kept.
-        // https://github.com/johnagan/clean-webpack-plugin
         new CleanWebpackPlugin(),
-        /*
-            * для Apache серверу потрібен index.php файл який ми дублюємо з index.html
-        */
-        new HtmlWebPackPlugin({
-            hash: false,
-            template: `${baseWebpackConfig.externals.paths.src}/index.html`,
-            filename: "./index.php"
-        }),
     ]
 });
 
--- src/app/config/core.js
+++ src/app/config/core.js
@@ -1,9 +1,4 @@
+import { publicPath } from '../../../config/constans/core';
+export const rootHref = publicPath;
+
 export const restapiexampleApi = 'http://dummy.restapiexample.com'
-
-// dev http://localhost:3000
-export const rootHref = '/'
-// build local
-// export const rootHref = './'
-
-// prod test
-// export const rootHref = '/drugstoreOnlineTest/';
